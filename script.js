30387c01dcd52957fc40a85ef37f0ef8548e4e28
luccaeustaquio <luccaeust@gmail.com>
2026-08-19 21:20:36 -0300

    Trabalhando na área de Livros

---
+++ script.js
@@ -0,0 +1,18 @@
+window.confirm("Voce deseja continuar a iteraçao?")
+
+ var nome_Pessoa = window.prompt("Qual é o seu nome?")
+ var estudante = window.prompt("Qual sua profissao?")
+
+  let confirm = confirm("Vc confirma essas informaçoes? ")
+  
+        if (confirm == "Sim" || confirm == "sim"){
+               window.alert("Que bom ter vc conosco ") 
+        }
+
+window.alert(" Nome: " + nome_Pessoa + " Profissao: "+ estudante )
+
+
+
+
+
+
